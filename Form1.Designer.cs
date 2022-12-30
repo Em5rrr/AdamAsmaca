37a59f248286c3bac41e00c53fbd6822f2252cec
unknown <emirsezai85@gmail.com>
2022-12-30 23:34:31 +0300

    Signed-off-by: unknown <emirsezai85@gmail.com>

---
+++ Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace adamAsmaca
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(508, 447);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Silver;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(661, 188);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 102);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Var Mı?";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(622, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "_";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(641, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "_";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(660, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "_";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(679, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "_";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(698, 99);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "_";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(717, 99);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(18, 20);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "_";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(741, 99);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(18, 20);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "_";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(555, 188);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Silver;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(661, 296);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 54);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "İpucu";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(877, 466);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AdamAsmacaGame";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace adamAsmaca
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int hak = 0;
+        string kelime = "FERRARI";
+        private void button1_Click(object sender, EventArgs e)
+        {                              
+                if (label1.Text == "F")
+                {
+                    if (label2.Text == "E")
+                    {
+                        if (label3.Text == "R")
+                        {
+                            if (label4.Text == "R")
+                            {
+                                if (label5.Text == "A")
+                                {
+                                    if (label6.Text == "R")
+                                    {
+                                        if (label7.Text == "I")
+                                        {
+                                            button1.Enabled = false;
+                                            textBox1.Enabled = false;
+                                            MessageBox.Show("Tebrikler Oyunu Kazandınız!");
+                                            Application.Exit();
+                                        }
+                                    }
+
+                                }
+                            }
+                        }
+                    }
+                }
+                textBox1.Text = textBox1.Text.ToUpper();
+                if (textBox1.Text == "")
+                {
+                    MessageBox.Show("Lütfen Bir Harf Girin");
+                }
+                else
+                {
+
+                    if (textBox1.Text == kelime[0].ToString())
+                    {
+                        label1.Text = "F";
+                    }
+                    else
+                    {
+                        if (textBox1.Text == kelime[1].ToString())
+                        {
+                            label2.Text = "E";
+                        }
+                        else
+                        {
+                            if (textBox1.Text == kelime[2].ToString())
+                            {
+                                label3.Text = "R";
+                                label4.Text = "R";
+                                label6.Text = "R";
+                            }
+                            else
+                            {
+                                if (textBox1.Text == kelime[4].ToString())
+                                {
+                                    label5.Text = "A";
+                                }
+                                else
+                                {
+                                    if (textBox1.Text == kelime[6].ToString())
+                                    {
+                                        label7.Text = "I";
+                                    }
+                                    else
+                                    {
+
+                                        hak++;
+                                    if (hak == 1)
+                                    {
+                                        pictureBox1.ImageLocation = "C:\\PROJECTS\\adamAsmaca\\Resources\\1.png";
+                                        MessageBox.Show("KALAN HAKKINIZ = 8");
+
+                                    }
+                                    else if (hak == 2)
+                                    {
+                                        pictureBox1.ImageLocation = "C:\\PROJECTS\\adamAsmaca\\Resources\\2.png";
+                                        MessageBox.Show("KALAN HAKKINIZ = 7");
+
+                                    }
+                                    else if (hak == 3)
+                                    {
+                                        pictureBox1.ImageLocation = "C:\\PROJECTS\\adamAsmaca\\Resources\\3.png";
+                                        MessageBox.Show("KALAN HAKKINIZ = 6");
+                                    }
+                                    else if (hak == 4)
+                                    {
+
+                                        pictureBox1.ImageLocation = "C:\\PROJECTS\\adamAsmaca\\Resources\\4.png";
+                                        MessageBox.Show("KALAN HAKKINIZ = 5");
+                                    }
+                                    else if (hak == 5)
+                                    {
+
+                                        pictureBox1.ImageLocation = "C:\\PROJECTS\\adamAsmaca\\Resources\\5.png";
+                                        MessageBox.Show("KALAN HAKKINIZ = 4");
+                                    }
+                                    else if (hak == 6)
+                                    {
+
+                                        pictureBox1.ImageLocation = "C:\\PROJECTS\\adamAsmaca\\Resources\\6.png";
+                                        MessageBox.Show("KALAN HAKKINIZ = 3");
+                                    }
+                                    else if (hak == 7)
+                                    {
+
+                                        pictureBox1.ImageLocation = "C:\\PROJECTS\\adamAsmaca\\Resources\\7.png";
+                                        MessageBox.Show("KALAN HAKKINIZ = 2");
+                                    }
+                                    else if (hak == 8)
+                                    {
+
+                                        pictureBox1.ImageLocation = "C:\\PROJECTS\\adamAsmaca\\Resources\\8.png";
+                                        MessageBox.Show("KALAN HAKKINIZ = 1");
+                                    }
+                                    else if (hak == 9)
+                                        {
+                                            pictureBox1.ImageLocation = "C:\\PROJECTS\\adamAsmaca\\Resources\\9.png";
+                                            button1.Enabled = false;
+                                            textBox1.Enabled = false;
+                                            MessageBox.Show("HAKKINIZ KALMADI");
+                                            MessageBox.Show("Oyunu Kaybettiniz!");
+                                            MessageBox.Show("Scuderia Ferrari", "Cevap");
+                                            Application.Exit();
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                textBox1.Clear();
+        }                  
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Adam Asmaca'ya hoşgeldiniz.");
+            pictureBox1.BackColor = Color.White;
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("İtalyan Araba Markası");
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
